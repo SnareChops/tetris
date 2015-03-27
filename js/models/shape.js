06fb4b43336c62ed77f6d3bf2ba6ec8ace537bb8
aric87 <aric87@comcast.net>
2015-03-27 11:34:26 -0400

    remove whitespace

--- js/models/shape.js
+++ js/models/shape.js
@@ -15,10 +15,10 @@ var Shape = function(shape){
 };
 
 var shapes = [];
-shapes.push({coordinates:[{x:0, y:0}, {x:1, y:0}, {x:2, y:0}, {x:3, y:0}],color:'#00f0f0'}); //Block 1
-	shapes.push({coordinates:[{x:0, y:0}, {x:0, y:1}, {x:1, y:0}, {x:2, y:0}],color:'#0000f0'}); //Block 2
-	shapes.push({coordinates:[{x:0, y:0}, {x:1, y:0}, {x:2, y:0}, {x:2, y:1}],color:'#f0a000'}); //Block 3
-	shapes.push({coordinates:[{x:0, y:0}, {x:0, y:1}, {x:1, y:0}, {x:1, y:1}],color:'#f0f000'}); //Block 4
-	shapes.push({coordinates:[{x:0, y:0}, {x:1, y:0}, {x:1, y:1}, {x:2, y:1}],color:'#f0f000'}); //Block 5
-	shapes.push({coordinates:[{x:0, y:0}, {x:1, y:0}, {x:1, y:1}, {x:2, y:0}],color:'#a0f000'}); //Block 6
-	shapes.push({coordinates:[{x:0, y:1}, {x:1, y:1}, {x:1, y:0}, {x:2, y:0}],color:'#f00000'}); //Block 7
+shapes.push({coordinates:[{x:0, y:0}, {x:1, y:0}, {x:2, y:0}, {x:3, y:0}],color:'#00f0f0'}); //straight block
+shapes.push({coordinates:[{x:0, y:0}, {x:0, y:1}, {x:1, y:0}, {x:2, y:0}],color:'#0000f0'}); //left angle
+shapes.push({coordinates:[{x:0, y:0}, {x:1, y:0}, {x:2, y:0}, {x:2, y:1}],color:'#f0a000'}); //right angle
+shapes.push({coordinates:[{x:0, y:0}, {x:0, y:1}, {x:1, y:0}, {x:1, y:1}],color:'#f0f000'}); //square
+shapes.push({coordinates:[{x:0, y:0}, {x:1, y:0}, {x:1, y:1}, {x:2, y:1}],color:'#f0f000'}); //right s block
+shapes.push({coordinates:[{x:0, y:0}, {x:1, y:0}, {x:1, y:1}, {x:2, y:0}],color:'#a0f000'}); //pyramid block
+shapes.push({coordinates:[{x:0, y:1}, {x:1, y:1}, {x:1, y:0}, {x:2, y:0}],color:'#f00000'}); //left z block
