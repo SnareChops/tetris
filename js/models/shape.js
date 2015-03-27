0b90ca8d939e6cef9dded6921207ea05c9848331
aric87 <aric87@comcast.net>
2015-03-27 11:20:18 -0400

    add colors to blocks

--- js/models/shape.js
+++ js/models/shape.js
@@ -3,9 +3,10 @@ Array.max = function( array ){
 };
 
 var Shape = function(shape){
+	console.log(shapes[shape].coordinates);
   var self = this;
-  self.coordinates = shapes[shape];
-  self.color = 'red';
+  self.coordinates = shapes[shape].coordinates;
+  self.color = shapes[shape].color;
   self.max = {
     x: Array.max(self.coordinates.map(function(coord){return coord.x;})),
     y: Array.max(self.coordinates.map(function(coord){return coord.y;}))
@@ -15,10 +16,10 @@ var Shape = function(shape){
 };
 
 var shapes = [];
-shapes.push([{x:0, y:0}, {x:1, y:0}, {x:2, y:0}, {x:3, y:0}]); //Block 1
-shapes.push([{x:0, y:0}, {x:0, y:1}, {x:1, y:0}, {x:2, y:0}]); //Block 2
-shapes.push([{x:0, y:0}, {x:1, y:0}, {x:2, y:0}, {x:2, y:1}]); //Block 3
-shapes.push([{x:0, y:0}, {x:0, y:1}, {x:1, y:0}, {x:1, y:1}]); //Block 4
-shapes.push([{x:0, y:0}, {x:1, y:0}, {x:1, y:1}, {x:2, y:1}]); //Block 5
-shapes.push([{x:0, y:0}, {x:1, y:0}, {x:1, y:1}, {x:2, y:0}]); //Block 6
-shapes.push([{x:0, y:1}, {x:1, y:1}, {x:1, y:0}, {x:2, y:0}]); //Block 7
+shapes.push({coordinates:[{x:0, y:0}, {x:1, y:0}, {x:2, y:0}, {x:3, y:0}],color:'#00f0f0'}); //Block 1
+	shapes.push({coordinates:[{x:0, y:0}, {x:0, y:1}, {x:1, y:0}, {x:2, y:0}],color:'#0000f0'}); //Block 2
+	shapes.push({coordinates:[{x:0, y:0}, {x:1, y:0}, {x:2, y:0}, {x:2, y:1}],color:'#f0a000'}); //Block 3
+	shapes.push({coordinates:[{x:0, y:0}, {x:0, y:1}, {x:1, y:0}, {x:1, y:1}],color:'#f0f000'}); //Block 4
+	shapes.push({coordinates:[{x:0, y:0}, {x:1, y:0}, {x:1, y:1}, {x:2, y:1}],color:'#f0f000'}); //Block 5
+	shapes.push({coordinates:[{x:0, y:0}, {x:1, y:0}, {x:1, y:1}, {x:2, y:0}],color:'#a0f000'}); //Block 6
+	shapes.push({coordinates:[{x:0, y:1}, {x:1, y:1}, {x:1, y:0}, {x:2, y:0}],color:'#f00000'}); //Block 7
